72d39d55803f3c9dfd010f35926a691fda8a47ad
walaTuwaiq <wala.radi.rae.js@tuwaiq.edu.sa>
2022-02-17 17:25:55 +0300

    handling error

---
+++ index.py
@@ -0,0 +1,14 @@
+
+def additoin(x, y):
+    x = 10
+    y = 20
+    print("Addition:", x + b)
+
+
+
+try:
+    additoin(10, 20)
+except Exception:
+    print("Not Defined Variables.")
+else:
+    print("the operation is successful.")
