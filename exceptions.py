6e364a3219149e9cf15a4c404b8c454d2d58e4dc
Ashraf-cs <ashraf.gh.cs@gmail.com>
2022-02-17 17:35:55 +0300

    First commit

---
+++ exceptions.py
@@ -0,0 +1,12 @@
+def additoin(x, y):
+    x = 10
+    y = 20
+    print("Addition:", x + b)
+
+
+try:
+    additoin(10, 20)
+except Exception as e:
+    print("There is an error:", e)
+else:
+    print("the operation is successful")
