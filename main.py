9684a590c15de67709e93167d8a82ed8c96e1c8a
ShahadAltuwijry <Shahad.abdallah.elq.js@tuwaiq.edu.sa>
2022-02-17 17:24:09 +0300

    finished lab

---
+++ main.py
@@ -0,0 +1,15 @@
+def additoin(x, y):
+
+    try:
+        x = 10
+        y = 20
+        print("Addition:", x + b)
+    except NameError as n :
+        print("you have an error:", n) 
+    else:
+        print("the operation is successful")
+            
+    
+
+
+additoin(10, 20)
