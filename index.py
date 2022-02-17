2289024ed7739a656431e6c6004b994b7dad11e3
Nouf112233 <Nouf.Mohamed.elq.js@tuwaiq.edu.sa>
2022-02-17 17:19:45 +0300

    handle the exception

---
+++ index.py
@@ -0,0 +1,16 @@
+def additoin(x, y):
+          x = 10
+          y = 20
+          print("Addition:", x + b)
+
+try:
+    additoin(10, 20)
+except Exception as e:
+    print("you have an error",e)
+else:
+    print("the operation is successful")
+
+#Find what type of exception is raised.?
+#syntax error
+
+
