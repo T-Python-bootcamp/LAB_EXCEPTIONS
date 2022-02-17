7ade7225efd078266670101f607f8e0b6bf93c52
Alanoudalmakhalafi <alanoudm540@gmail.com>
2022-02-17 17:28:47 +0300

    exception lab done

---
+++ exceptionLab.py
@@ -0,0 +1,26 @@
+# def d(x):
+#     x/0
+#     print("h")
+# try:
+#     d(4)
+# except:
+#     print("u have error")
+# else:
+#     print("function complited")
+#     #finally 
+# print("program is running ")
+
+#####################################
+
+
+def additoin(x, y):
+          x = 10
+          y = 20
+          print("Addition:", x + b)
+
+try:
+ additoin(10, 20)
+except Exception as e:
+    print("You Have Error: ",e)
+else:
+    print("the operation is successful")
