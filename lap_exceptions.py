4d478fd030624b93190cf7abd8e3a6e971e82700
RawanZaben <rawan.zaben.rae.js@tuwaiq.edu.sa>
2022-02-24 10:56:55 +0300

    lap exceptions

---
+++ lap_exceptions.py
@@ -0,0 +1,10 @@
+def additoin(x, y):
+  try:
+          x = 10
+          y = 20
+          print("Addition :", x + b)
+  except Exception :
+        raise ValueError("variable not defined")
+  else:
+        print("the operation is successful")
+additoin(10, 20) 
